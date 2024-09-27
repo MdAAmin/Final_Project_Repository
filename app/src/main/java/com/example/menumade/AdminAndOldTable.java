05dc0df7b17a9761c31f31e6540ceb6e1f946a6b
MdAAmin <mdabdulamin11@gmail.com>
2024-09-27 19:57:35 +0530

    5th commit

--- app/src/main/java/com/example/menumade/AdminAndOldTable.java
+++ app/src/main/java/com/example/menumade/AdminAndOldTable.java
@@ -1,7 +1,11 @@
 package com.example.menumade;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -17,6 +21,13 @@ public class AdminAndOldTable extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_and_old_table);
 
+        LayoutInflater inflater = getLayoutInflater();
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) View layout = inflater.inflate(R.layout.toaster, findViewById(R.id.go));
+
+        Toast customToast = new Toast(getApplicationContext());
+        customToast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        customToast.setDuration(Toast.LENGTH_SHORT);
+        customToast.setView(layout);
         databaseHelper = new DatabaseHelper(this);
 
         tableNameEditText = findViewById(R.id.AD_ed);
@@ -30,6 +41,7 @@ public class AdminAndOldTable extends AppCompatActivity {
             String tableCapacityStr = tableCapacityEditText.getText().toString();
 
             if (!tableName.isEmpty() && !tableNumber.isEmpty() && !tableCapacityStr.isEmpty()) {
+
                 int tableCapacity = Integer.parseInt(tableCapacityStr);
                 databaseHelper.addTable(tableName, tableNumber, tableCapacity);
                 Toast.makeText(AdminAndOldTable.this, "Table Added Successfully", Toast.LENGTH_SHORT).show();
@@ -46,12 +58,14 @@ public class AdminAndOldTable extends AppCompatActivity {
 
         Button adminButton = findViewById(R.id.BTN_ADMIN);
         adminButton.setOnClickListener(v -> {
+            customToast.show();
             Intent intent1 = new Intent(AdminAndOldTable.this, AdminLogin.class);
             startActivity(intent1);
         });
 
         Button backButton = findViewById(R.id.BTN_Back);
         backButton.setOnClickListener(v -> {
+
             Intent intent3 = new Intent(AdminAndOldTable.this, MainActivity.class);
             startActivity(intent3);
         });
